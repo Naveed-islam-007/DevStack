0c7966c51f2d445f60ed4b30a8bfc3ab48b9ac26
Naveed-islam-007 <islamnaveed631@gmail.com>
2026-09-12 21:19:37 +0600

    third

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Logo from "../assets/logo-text.png";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm px-4 lg:px-8">
+        <div className="navbar bg-base-100 shadow-sm px-4 lg:px-8  sticky top-0 z-50">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
